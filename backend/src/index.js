9b646b63d69a48017b4c4fb62eee1f3b2239c5dd
kalvium <sanya.thakur@kalvium.community>
2025-03-07 09:52:15 +0530

    setting up basic express

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const cors=require("cors");
+const mongoose=require("mongoose");
+require("dotenv").config();
+const PORT=3010;
+const app=express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("Database Connected."))
+.catch((err)=>console.log("Error connecting to database. Error: ",err));
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
